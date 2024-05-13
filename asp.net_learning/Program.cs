d5aec2a73ea12123c59b9e95d6135d9c67d8b827
Srilatha <ysrilatha0308@gmail.com>
2024-05-13 20:46:30 +0200

    test commit

--- asp.net_learning/Program.cs
+++ asp.net_learning/Program.cs
@@ -7,6 +7,15 @@ namespace asp.net_learning
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Srilatha, welcome!");
+            Console.WriteLine("Hello Srilatha, welcome!");
+
+            Console.WriteLine("Hello Srilatha, welcome!");
+
+            Console.WriteLine("Hello Srilatha, welcome!");
+
+            Console.WriteLine("Hello Srilatha, welcome!");
+
         }
     }
 }
